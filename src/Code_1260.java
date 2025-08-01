f4bb115660c03e457552e66f4f3b33c02a336154
엄경문 <eomkyeongmun@naver.com>
2025-08-01 15:41:21 +0900

    문제 추가(BFS)

--- src/Code_1260.java
+++ src/Code_1260.java
@@ -1,4 +1,85 @@
-package PACKAGE_NAME;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+//DFS와 BFS
+//
+//시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+//2 초	128 MB	339045	136835	80760	38.886%
+//문제
+//그래프를 DFS로 탐색한 결과와 BFS로 탐색한 결과를 출력하는 프로그램을 작성하시오. 단, 방문할 수 있는 정점이 여러 개인 경우에는 정점 번호가 작은 것을 먼저 방문하고, 더 이상 방문할 수 있는 점이 없는 경우 종료한다. 정점 번호는 1번부터 N번까지이다.
+//
+//입력
+//첫째 줄에 정점의 개수 N(1 ≤ N ≤ 1,000), 간선의 개수 M(1 ≤ M ≤ 10,000), 탐색을 시작할 정점의 번호 V가 주어진다. 다음 M개의 줄에는 간선이 연결하는 두 정점의 번호가 주어진다. 어떤 두 정점 사이에 여러 개의 간선이 있을 수 있다. 입력으로 주어지는 간선은 양방향이다.
+//
+//출력
+//첫째 줄에 DFS를 수행한 결과를, 그 다음 줄에는 BFS를 수행한 결과를 출력한다. V부터 방문된 점을 순서대로 출력하면 된다.
+//실버2
 public class Code_1260 {
+    static boolean []visited;
+
+    static void DFS(int V, ArrayList<Integer>[] A) {
+        visited[V] = true;
+        if (visited[V]) {
+            System.out.print(V + " ");
+        }
+        for (int i : A[V]) {
+            if (!visited[i]) {
+                DFS(i, A);
+            }
+        }
+
+    }
+
+    static void BFS(int V,ArrayList<Integer>[] A){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(V);
+        visited[V] = true;
+        System.out.print("\n");
+        while (!queue.isEmpty()) {
+            int out = queue.poll();
+            System.out.print(out+" ");
+
+            for (int n : A[out]) {
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int v=  Integer.parseInt(st.nextToken());
+        ArrayList<Integer>[] A= new ArrayList[n+1];
+        for(int i=0;i<n+1;i++){
+            A[i]=new ArrayList<Integer>();
+        }
+
+        visited = new boolean[n+1];
+
+        for(int i=0;i<m;i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            A[a].add(b);
+            A[b].add(a);
+        }
+        for (int i=0;i<=n;i++) {
+            Collections.sort(A[i]);
+        }
+        for(int i=0;i<n+1;i++){
+            visited[i] = false;
+        }
+        DFS(v,A);
+        for(int i=0;i<n+1;i++){
+            visited[i] = false;
+        }
+        BFS(v,A);
+    }
 }
--- src/Code_2178.java
+++ src/Code_2178.java
@@ -1,4 +1,77 @@
-package PACKAGE_NAME;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+//미로 탐색
+//
+//시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+//1 초	192 MB	244517	115571	72224	45.561%
+//문제
+//N×M크기의 배열로 표현되는 미로가 있다.
+//
+//1	0	1	1	1	1
+//        1	0	1	0	1	0
+//        1	0	1	0	1	1
+//        1	1	1	0	1	1
+//미로에서 1은 이동할 수 있는 칸을 나타내고, 0은 이동할 수 없는 칸을 나타낸다. 이러한 미로가 주어졌을 때, (1, 1)에서 출발하여 (N, M)의 위치로 이동할 때 지나야 하는 최소의 칸 수를 구하는 프로그램을 작성하시오. 한 칸에서 다른 칸으로 이동할 때, 서로 인접한 칸으로만 이동할 수 있다.
+//
+//위의 예에서는 15칸을 지나야 (N, M)의 위치로 이동할 수 있다. 칸을 셀 때에는 시작 위치와 도착 위치도 포함한다.
+//
+//입력
+//첫째 줄에 두 정수 N, M(2 ≤ N, M ≤ 100)이 주어진다. 다음 N개의 줄에는 M개의 정수로 미로가 주어진다. 각각의 수들은 붙어서 입력으로 주어진다.
+//
+//        출력
+//첫째 줄에 지나야 하는 최소의 칸 수를 출력한다. 항상 도착위치로 이동할 수 있는 경우만 입력으로 주어진다.
+//다시풀기 실버1
 public class Code_2178 {
+    static boolean [][] visited;
+    static int[][] a;
+    static int[][] dist;
+    static int n, m;
+    //상하좌우
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    static void BFS(int x,int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        visited[x][y] = true;
+        dist[x][y] = 1;
+        queue.add(new int[]{x, y});
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            int now_x = now[0];
+            int now_y = now[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = now_x + dx[i];
+                int ny = now_y + dy[i];
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                    if (!visited[nx][ny] && a[nx][ny] == 1) {
+                        visited[nx][ny] = true;
+                        dist[nx][ny] = dist[now_x][now_y] + 1;
+                        queue.add(new int[]{nx, ny});
+                    }
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+         n = Integer.parseInt(st.nextToken());
+         m = Integer.parseInt(st.nextToken());
+         a= new int[n][m];
+        dist = new int[n][m];
+        for(int i=0;i<n;i++){
+            String line = br.readLine();
+            for(int j=0;j<m;j++){
+              a[i][j] = line.charAt(j)-'0';
+            }
+        }
+        visited=new boolean[n][m];
+        BFS(0, 0);
+        System.out.println(dist[n - 1][m - 1]);
+    }
 }
